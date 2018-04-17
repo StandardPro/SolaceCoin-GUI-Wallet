52a38a992f31b48109896d64f3f06c52415fce59
ingerulro <elektryon.dev@gmail.com>
2018-04-17 23:14:02 +0300

    Change

--- html/index.py
+++ html/index.py
@@ -516,14 +516,8 @@ html ="""
                                                             'tx_status': tx_status_text,
                                                             'tx_fa_icon': tx['direction'] == "in" ? "mail-forward" : "reply",
                                                             'tx_id': tx['txid'],
-<<<<<<< HEAD
                                                             'tx_payment_id': tx['payment_id'], 
                                                             'tx_amount': printMoney(tx['amount']/10000000.),
-=======
-                                                            'tx_payment_id': tx['payment_id'],
-                                                            'tx_amount': printMoney(tx['amount']
-                                                            0000000.),
->>>>>>> 7938ea28847730db37f27d37d4c231cd4bf27b0b
                                                             'tx_fee': printMoney(tx['fee']/10000000.),
                                                             'tx_fee_hide': tx['fee'] > 0 ? '' : 'tx-fee-hide',
                                                             'tx_date': dateConverter(tx['timestamp']),
@@ -690,11 +684,7 @@ html ="""
                                                             'tx_status': tx_status_text,
                                                             'tx_fa_icon': tx['direction'] == "in" ? "mail-forward" : "reply",
                                                             'tx_id': tx['txid'],
-<<<<<<< HEAD
                                                             'tx_payment_id': tx['payment_id'], 
-=======
-                                                            'tx_payment_id': tx['payment_id'],
->>>>>>> 7938ea28847730db37f27d37d4c231cd4bf27b0b
                                                             'tx_amount': printMoney(tx['amount']/10000000.),
                                                             'tx_fee': printMoney(tx['fee']/10000000.),
                                                             'tx_fee_hide': tx['fee'] > 0 ? '' : 'tx-fee-hide',
